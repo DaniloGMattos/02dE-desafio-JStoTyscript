f7151901fae61f6fb772e10bb175c3e766eb6119
DaniloGMattos <mattosgdanilo@gmail.com>
2021-07-07 18:01:44 -0300

    [FIX] Desafio completo

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,5 +1,8 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from "styled-components";
 
+interface ContainerProps {
+  isAvailable: boolean;
+}
 export const Container = styled.div`
   background: #f0f0f5;
   border-radius: 8px;
@@ -12,9 +15,9 @@ export const Container = styled.div`
     transition: 0.3s opacity;
     text-align: center;
 
-    ${props =>
-    !props.available &&
-    css`
+    ${(props: ContainerProps) =>
+      !props.isAvailable &&
+      css`
         opacity: 0.3;
       `};
 
@@ -114,7 +117,7 @@ export const Container = styled.div`
 
           &:before {
             position: absolute;
-            content: '';
+            content: "";
             height: 20px;
             width: 40px;
             left: 8px;
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,20 @@ import {
   useRef,
   useState,
   useCallback,
-} from 'react';
+  InputHTMLAttributes,
+} from "react";
+import { IconType } from "react-icons";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: IconType;
+}
+
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,14 +31,14 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current);
   }, []);
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
@@ -48,6 +55,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
